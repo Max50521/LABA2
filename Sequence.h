432e5d8f8daf9b34c8fa191cbbcf70f72b76f768
Max50521 <71272440+Max50521@users.noreply.github.com>
2021-06-16 13:54:26 +0300

    Add files via upload

---
+++ Sequence.h
@@ -0,0 +1,29 @@
+
+#ifndef MAT_SEQUENCE_H
+#define MAT_SEQUENCE_H
+
+#endif
+
+#ifndef UNTITLED_SEQUENCE_H
+#define UNTITLED_SEQUENCE_H
+
+#endif 
+template <typename T> class Sequence {
+public:
+    //decompose
+    virtual int GetSize() const = 0;
+    virtual T Get(int index) const = 0;
+    virtual T GetLast() const = 0;
+    virtual T GetFirst() const = 0;
+    virtual Sequence<T>* GetSubSequence(int start, int end) = 0;
+
+    //operations
+    virtual void Set(int index, T item) = 0;
+    virtual Sequence<T>* Concat(Sequence<T>* list) = 0;
+    virtual void Append(T item) = 0;
+    virtual void Prepend(T item) = 0;
+    virtual void InsertAt(T item, int index) = 0;
+    // operators
+    virtual T operator[](const int index) = 0;
+
+};
